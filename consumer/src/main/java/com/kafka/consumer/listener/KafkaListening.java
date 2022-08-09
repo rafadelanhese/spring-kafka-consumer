096fac3b6d1e3c7b4b429bf152b6dc4018d346b6
Rafael Delanhese <rafael.delanhesemachado@everis.nttdata.com>
2022-08-09 17:55:59 -0300

    initial commit

---
+++ consumer/src/main/java/com/kafka/consumer/listener/KafkaListening.java
@@ -0,0 +1,24 @@
+package com.kafka.consumer.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Slf4j
+@Service
+public class KafkaListening {
+
+    @Value("${topic.name.consumer}")
+    private String topicName;
+
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload){
+        log.info("Tópico: {}", topicName);
+        log.info("key: {}", payload.key());
+        log.info("Headers: {}", payload.headers());
+        log.info("Partion: {}", payload.partition());
+        log.info("Order: {}", payload.value());
+    }
+}
